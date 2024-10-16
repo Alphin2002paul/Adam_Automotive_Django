6cd90d0192675db8265dce94a03dda8138bac83a
Alphin Paul Saji <77055479+Alphin2002paul@users.noreply.github.com>
2024-10-16 17:48:05 +0530

    render.yaml

--- AdamAuto/AdamAuto/settings.py
+++ AdamAuto/AdamAuto/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AdamAuto.urls'
@@ -187,3 +188,11 @@ SOCIALACCOUNT_LOGIN_ON_GET=True
 
 # settings.py
 LOGIN_URL = '/login/'
+
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- AdamAuto/build.sh
+++ AdamAuto/build.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -5,4 +6,3 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
