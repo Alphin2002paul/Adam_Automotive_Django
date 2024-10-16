d263c21f9f8fd522d06c471e9c6ec1e9bd10c099
Alphin Paul Saji <77055479+Alphin2002paul@users.noreply.github.com>
2024-10-16 19:02:05 +0530

    hosts

--- AdamAuto/AdamAuto/settings.py
+++ AdamAuto/AdamAuto/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*,.up.railway.app"]
 
 
 # Application definition
